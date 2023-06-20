df2c95e20953881075177d6fad928e57c8dd4b68
Gnoompa <gnoompa@gmail.com>
2023-06-20 21:01:12 +0400

    test

--- app/api/suggestion/route.ts
+++ app/api/suggestion/route.ts
@@ -1,13 +1,18 @@
-export async function GET(request:Request) {
-    // Connect to our Microsoft Azure Function Endpoint
-    const response = await fetch("https://ai-image-generator-ytb-app.azurewebsites.net/api/getchatgptsuggestion", {
-        cache: 'no-store'
-    }) 
+export async function GET(request: Request) {
+  // Connect to our Microsoft Azure Function Endpoint
+  const response = await fetch(
+    "https://ai-image-generator-ytb-app.azurewebsites.net/api/getchatgptsuggestion",
+    {
+      headers: {
+        "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate",
+      },
+      cache: "no-store"
+    }
+  );
 
-    const textData = await response.text();
-    console.log(textData)
-    return new Response(JSON.stringify(textData.trim()), {
-        status:200,
-    });
-   
+  const textData = await response.text();
+  console.log(textData);
+  return new Response(JSON.stringify(textData.trim()), {
+    status: 200,
+  });
 }
