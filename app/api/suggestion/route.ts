8ef457611175bf551264639c0bebca42ae43831e
Gnoompa <gnoompa@gmail.com>
2023-06-20 21:07:10 +0400

    test

--- app/api/suggestion/route.ts
+++ app/api/suggestion/route.ts
@@ -3,10 +3,13 @@ export async function GET(request: Request) {
   const response = await fetch(
     "https://ai-image-generator-ytb-app.azurewebsites.net/api/getchatgptsuggestion",
     {
-      headers: {
-        "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate",
+      //   headers: {
+      //     "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate",
+      //   },
+      cache: "no-store",
+      next: {
+        revalidate: 0,
       },
-      cache: "no-store"
     }
   );
 
